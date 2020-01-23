10e50b48e0f490d1ffeab938ccf27ccb43fa50d0
demola07 <45898468+demola07@users.noreply.github.com>
2020-01-23 12:24:15 +0100

    Initialize firebase cloud functions

---
+++ functions/index.js
@@ -0,0 +1,71 @@
+/* Visit https://firebase.google.com/docs/database/security to learn more about security rules. */
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+const serviceAccount = require('./utils/serviceAcctKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://socialapp-c82fa.firebaseio.com'
+});
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
+});
+
+// exports.getScreams = functions.https.onRequest((req, res) => {
+//   admin
+//     .firestore()
+//     .collection('screams')
+//     .get()
+//     .then(data => {
+//       let screams = [];
+//       data.forEach(doc => {
+//         screams.push(doc.data());
+//       });
+//       return res.json(screams);
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
+// });
+
+exports.getScreams = functions.https.onRequest(async (req, res) => {
+  try {
+    const data = await admin
+      .firestore()
+      .collection('screams')
+      .get();
+    let screams = [];
+    data.forEach(doc => {
+      screams.push(doc.data());
+    });
+    return res.json(screams);
+  } catch (error) {
+    console.error(error);
+    ('something went wrong');
+  }
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      res.json({
+        message: `Document ${doc.id} created successfully`
+      });
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'something went wrong' });
+      console.error(error);
+    });
+});
