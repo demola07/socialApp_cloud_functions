4aec2b887e8dcb2b85cd63da7cc2422892d935a5
demola07 <45898468+demola07@users.noreply.github.com>
2020-03-19 22:48:09 +0100

    list commemts starting with the most recent

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -54,6 +54,7 @@ exports.getScream = (req, res) => {
       screamData.screamId = doc.id;
       return db
         .collection('comments')
+        .orderBy('createdAt', 'desc')
         .where('screamId', '==', req.params.screamId)
         .get();
     })
