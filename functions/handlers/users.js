b34ab3ee0b232b64ccaf69c3c88c2ead54dc0680
demola07 <45898468+demola07@users.noreply.github.com>
2020-03-22 01:49:54 +0100

    created route to retrieve a particular users details via the users handle

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -112,6 +112,45 @@ exports.addUserDetails = (req, res) => {
     });
 };
 
+//Get any user's Details
+exports.getUserDetails = (req, res) => {
+  let userData = {};
+
+  db.doc(`/users/${req.params.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        userData.user = doc.data();
+        return db
+          .collection('screams')
+          .where('userHandle', '==', req.params.handle)
+          .orderBy('createdAt', 'desc')
+          .get();
+      } else {
+        return res.status(404).json({ error: 'User not found' });
+      }
+    })
+    .then(data => {
+      userData.screams = [];
+      data.forEach(doc => {
+        userData.screams.push({
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+          userHandle: doc.data().userHandle,
+          userImage: doc.data().userImage,
+          likeCount: doc.data().likeCount,
+          commentCount: doc.data().commentCount,
+          screamId: doc.id
+        });
+      });
+      return res.json(userData);
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
 //Get own user Details
 exports.getAuthenticatedUser = (req, res) => {
   let userData = {};
