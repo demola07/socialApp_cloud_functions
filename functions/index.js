83805c93bd4d75d3e98d1281a324aba83c2927f8
demola07 <45898468+demola07@users.noreply.github.com>
2020-03-22 17:57:19 +0100

    Ignore notification on like and comment from same user that is posting it

--- functions/index.js
+++ functions/index.js
@@ -47,10 +47,14 @@ exports.createNotificationOnLike = functions
   .region('europe-west1')
   .firestore.document('likes/{id}')
   .onCreate(snapshot => {
-    db.doc(`/screams/${snapshot.data().screamId}`)
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
       .get()
       .then(doc => {
-        if (doc.exists) {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
           return db.doc(`/notifications/${snapshot.id}`).set({
             createdAt: new Date().toISOString(),
             recipient: doc.data().userHandle,
@@ -61,12 +65,8 @@ exports.createNotificationOnLike = functions
           });
         }
       })
-      .then(() => {
-        return;
-      })
       .catch(err => {
         console.error(err);
-        return;
       });
   });
 
@@ -74,11 +74,10 @@ exports.deleteNotificationOnUnlike = functions
   .region('europe-west1')
   .firestore.document('likes/{id}')
   .onDelete(snapshot => {
-    db.doc(`/notifications/${snapshot.id}`)
+    return db
+      .doc(`/notifications/${snapshot.id}`)
       .delete()
-      .then(() => {
-        return;
-      })
+
       .catch(err => {
         console.error(err);
         return;
@@ -89,10 +88,14 @@ exports.createNotificationOnComment = functions
   .region('europe-west1')
   .firestore.document('comments/{id}')
   .onCreate(snapshot => {
-    db.doc(`/screams/${snapshot.data().screamId}`)
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
       .get()
       .then(doc => {
-        if (doc.exists) {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
           return db.doc(`/notifications/${snapshot.id}`).set({
             createdAt: new Date().toISOString(),
             recipient: doc.data().userHandle,
@@ -102,9 +105,6 @@ exports.createNotificationOnComment = functions
             screamId: doc.id
           });
         }
-      })
-      .then(() => {
-        return;
       })
       .catch(err => {
         console.error(err);
