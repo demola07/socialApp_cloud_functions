caffbe88afeaa431872e87d4253e15db4ccf6853
demola07 <45898468+demola07@users.noreply.github.com>
2020-03-22 17:46:55 +0100

    Changed error return message in signup and login routes

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -61,7 +61,9 @@ exports.signup = (req, res) => {
       if (error.code === 'auth/email-already-in-use') {
         return res.status(400).json({ email: 'Email already in use' });
       } else {
-        return res.status(500).json({ error: error.code });
+        return res
+          .status(500)
+          .json({ general: 'Something went wront... Please try again' });
       }
     });
 };
@@ -87,13 +89,9 @@ exports.login = (req, res) => {
     })
     .catch(err => {
       console.error(err);
-      if (err.code === 'auth/wrong-password') {
-        return res
-          .status(403)
-          .json({ general: 'Wrong credentials...Please try again' });
-      } else {
-        return res.status(500).json({ error: err.code });
-      }
+      return res
+        .status(403)
+        .json({ general: 'Wrong credentials...Please try again' });
     });
 };
 
