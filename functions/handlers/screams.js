c4ef56852cf9b5b4fb41c8e1fe733704141b7ee4
demola07 <45898468+demola07@users.noreply.github.com>
2020-03-22 17:51:29 +0100

    Changed error message for commentOnScream route

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -78,7 +78,7 @@ exports.getScream = (req, res) => {
 // Comment on a Scream
 exports.commentOnScream = (req, res) => {
   if (req.body.body.trim() === '')
-    return res.status(400).json({ error: 'Must not be empty' });
+    return res.status(400).json({ comment: 'Must not be empty' });
 
   const newComment = {
     body: req.body.body,
